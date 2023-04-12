9b272348e3de05c2dee275e7cfcb2d4913006563
Third <iamzedthird@gmail.com>
2023-04-12 20:08:25 +0700

    Form 3 (Data Loading CSV done)

--- Form2.vb
+++ Form2.vb
@@ -1,7 +1,8 @@
 ﻿Public Class Form2
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Panel2.Visible = True
-
+        Dim form3 As New Form3()
+        form3.Show()
     End Sub
 
     Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
---
+++ Form3.vb
@@ -0,0 +1,22 @@
+﻿Imports System.IO
+Imports System.Data.OleDb
+
+Public Class Form3
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim openFileDialog1 As New OpenFileDialog()
+
+        openFileDialog1.Filter = "CSV Files (*.csv)|*.csv"
+        openFileDialog1.FilterIndex = 1
+        openFileDialog1.Multiselect = False
+
+        If openFileDialog1.ShowDialog() = DialogResult.OK Then
+            Dim fileName As String = openFileDialog1.FileName
+            Dim connectionString As String = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" &
+                Path.GetDirectoryName(fileName) & ";Extended Properties='Text;HDR=YES;'"
+            Dim adapter As New OleDbDataAdapter("SELECT * FROM [" & Path.GetFileName(fileName) & "]", connectionString)
+            Dim ds As New DataSet()
+            adapter.Fill(ds, "data")
+            DataGridView1.DataSource = ds.Tables("data")
+        End If
+    End Sub
+End Class
