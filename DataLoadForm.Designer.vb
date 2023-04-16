b0e5dd7c3e4d032bcbab279141bf7035e7802592
Third <iamzedthird@gmail.com>
2023-04-16 15:17:49 +0700

    EDP act 5 done

--- DataLoadForm.Designer.vb
+++ DataLoadForm.Designer.vb
@@ -24,34 +24,45 @@ Partial Class DataLoadForm
     Private Sub InitializeComponent()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.saveButton = New System.Windows.Forms.Button()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'DataGridView1
         '
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(28, 134)
+        Me.DataGridView1.Location = New System.Drawing.Point(30, 85)
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.Size = New System.Drawing.Size(740, 279)
         Me.DataGridView1.TabIndex = 0
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(42, 35)
+        Me.Button1.Location = New System.Drawing.Point(30, 33)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(101, 33)
         Me.Button1.TabIndex = 1
         Me.Button1.Text = "ReadData"
         Me.Button1.UseVisualStyleBackColor = True
         '
-        'Form3
+        'saveButton
+        '
+        Me.saveButton.Location = New System.Drawing.Point(669, 381)
+        Me.saveButton.Name = "saveButton"
+        Me.saveButton.Size = New System.Drawing.Size(101, 33)
+        Me.saveButton.TabIndex = 2
+        Me.saveButton.Text = "Save Data"
+        Me.saveButton.UseVisualStyleBackColor = True
+        '
+        'DataLoadForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.saveButton)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.DataGridView1)
-        Me.Name = "Form3"
+        Me.Name = "DataLoadForm"
         Me.Text = "Form3"
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
@@ -60,4 +71,5 @@ Partial Class DataLoadForm
 
     Friend WithEvents DataGridView1 As DataGridView
     Friend WithEvents Button1 As Button
+    Friend WithEvents saveButton As Button
 End Class
--- DataLoadForm.vb
+++ DataLoadForm.vb
@@ -1,8 +1,6 @@
 ﻿Imports System.IO
 Imports System.Data.OleDb
-
 Imports MySql.Data.MySqlClient
-
 
 Public Class DataLoadForm
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
@@ -16,40 +14,63 @@ Public Class DataLoadForm
             Dim fileName As String = openFileDialog1.FileName
 
             Try
-                Connect_to_DB()
+                Dim dt As New DataTable()
 
-                Dim adapter As New MySqlDataAdapter()
-                Dim cmd As New MySqlCommand("INSERT INTO client(id, last_name, first_name, age, sex, address, program_id) " &
-                                            "VALUES(@id, @last_name, @first_name, @age, @sex, @address, @program_id)", myconn)
-                cmd.Parameters.AddWithValue("@id", "")
-                cmd.Parameters.AddWithValue("@last_name", "")
-                cmd.Parameters.AddWithValue("@first_name", "")
-                cmd.Parameters.AddWithValue("@age", "")
-                cmd.Parameters.AddWithValue("@sex", "")
-                cmd.Parameters.AddWithValue("@address", "")
-                cmd.Parameters.AddWithValue("@program_id", "")
-                adapter.InsertCommand = cmd
+                ' Read data from CSV file into DataTable
+                Using cn As New OleDbConnection With {
+                    .ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & Path.GetDirectoryName(fileName) & ";Extended Properties='text;HDR=YES;FMT=Delimited'"
+                }
+                    Using da As New OleDbDataAdapter("SELECT * FROM [" & Path.GetFileName(fileName) & "]", cn)
+                        da.Fill(dt)
+                    End Using
+                End Using
 
-                Dim lines As String() = File.ReadAllLines(fileName)
-                For i As Integer = 1 To lines.Length - 1
-                    Dim parts As String() = lines(i).Split(","c)
-                    cmd.Parameters("@id").Value = parts(0)
-                    cmd.Parameters("@last_name").Value = parts(1)
-                    cmd.Parameters("@first_name").Value = parts(2)
-                    cmd.Parameters("@age").Value = parts(3)
-                    cmd.Parameters("@sex").Value = parts(4)
-                    cmd.Parameters("@address").Value = parts(5)
-                    cmd.Parameters("@program_id").Value = parts(6)
-                    adapter.InsertCommand.ExecuteNonQuery()
-                Next
+                ' Bind DataTable to DataGridView
+                DataGridView1.DataSource = dt
+                DataGridView1.AutoResizeColumns()
 
-                Disconnect_to_DB()
-                MessageBox.Show("Data saved to database successfully.", "Save to Database", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                ' Show save button
+                saveButton.Enabled = True
+
             Catch ex As Exception
-                Disconnect_to_DB()
-                MessageBox.Show("Error: " & ex.Message, "Save to Database", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                MessageBox.Show("Error: " & ex.Message, "Load from CSV", MessageBoxButtons.OK, MessageBoxIcon.Error)
             End Try
         End If
+    End Sub
+
+    Private Sub saveButton_Click(sender As Object, e As EventArgs) Handles saveButton.Click
+        Try
+            Connect_to_DB()
+
+            Dim adapter As New MySqlDataAdapter()
+            Dim cmd As New MySqlCommand("INSERT INTO client(client_id, last_name, first_name, age, sex, address, program_id) " &
+                                        "VALUES(@client_id, @last_name, @first_name, @age, @sex, @address, @program_id)", myconn)
+            cmd.Parameters.AddWithValue("@client_id", "")
+            cmd.Parameters.AddWithValue("@last_name", "")
+            cmd.Parameters.AddWithValue("@first_name", "")
+            cmd.Parameters.AddWithValue("@age", "")
+            cmd.Parameters.AddWithValue("@sex", "")
+            cmd.Parameters.AddWithValue("@address", "")
+            cmd.Parameters.AddWithValue("@program_id", "")
+            adapter.InsertCommand = cmd
+
+            For Each row As DataGridViewRow In DataGridView1.Rows
+                cmd.Parameters("@client_id").Value = row.Cells(0).Value
+                cmd.Parameters("@last_name").Value = row.Cells(1).Value
+                cmd.Parameters("@first_name").Value = row.Cells(2).Value
+                cmd.Parameters("@age").Value = row.Cells(3).Value
+                cmd.Parameters("@sex").Value = row.Cells(4).Value
+                cmd.Parameters("@address").Value = row.Cells(5).Value
+                cmd.Parameters("@program_id").Value = row.Cells(6).Value
+                adapter.InsertCommand.ExecuteNonQuery()
+            Next
+
+            Disconnect_to_DB()
+            MessageBox.Show("Data saved to database successfully.", "Save to Database", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Catch ex As Exception
+            Disconnect_to_DB()
+            MessageBox.Show("Error: " & ex.Message, "Save to Database", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
     End Sub
 End Class
 
